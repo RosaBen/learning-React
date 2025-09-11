402c10d3936805793574c9dafbb18c0cd2c48f3a
RosaBen <benchabane.rosa@outlook.com>
2025-09-11 13:06:38 +0200

    added css, and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import cssPlugin from "@eslint/css";
+import markdown from "@eslint/markdown";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
+    files: ["**/*.css"],
+    plugins: { css: cssPlugin },
+    language: "css/css",
+    extends: ["css/recommended"],  // règles recommandées CSS
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "css/no-duplicate-imports": "error",
+      "css/no-empty-blocks": "warn",
     },
   },
-])
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    processor: "markdown/markdown",
+  },
+]);
--- src/components/hangman/App.jsx
+++ src/components/hangman/App.jsx
@@ -2,19 +2,37 @@ import { useState } from "react";
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
+       const alphabet = "abcdefghijklmnopqrstuvwxyz"
   const chips = languages.map(lang => {
         const styles = {
             backgroundColor: lang.backgroundColor,
             color: lang.color
         }
         return (
-            <span style={styles} className="chip">{lang.name}</span>
+            <span style={styles} key={lang.name} className="chip">{lang.name}</span>
         )
     })
- 
     const [currentWord] = useState("Hello")
+    const [guessed, setGuessed] = useState([]);
+    // const selectedKey = (letter) => {
+    // setGuessed(prevGuessed => prevGuessed.includes(letter)? prevGuessed: [...prevGuessed,letter])
+    // };
+
+    const selectedKey = (letter)=>{
+      setGuessed(prev => {
+        const letters = new Set(prev)
+        letters.add(letter)
+        return Array.from(letters)
+      })
+    }
+    console.log(guessed);
+    const keyboard = alphabet.split('').map(letter =>
+      <button key={letter} className="keyboard-letter" onClick={() => selectedKey(letter)}>{letter.toUpperCase()}</button>
+    );
     const word = [...currentWord.toUpperCase()]
     const letters = word.map((letter, index) => <span key={index} className="letter">{letter}</span>)
+
+
     return (
         <main>
             <header>
@@ -30,6 +48,9 @@ export default function AssemblyEndgame() {
               </section>
               <section className="word">
                 {letters}
+              </section>
+              <section className="keyboard">
+                {keyboard}
               </section>
         </main>
     )
